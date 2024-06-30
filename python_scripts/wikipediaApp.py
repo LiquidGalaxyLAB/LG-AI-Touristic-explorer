abd523ad39555fabce83abbdd4117e5ca415849d
Manas Dalvi <dalvimanas33@gmail.com>
2024-07-01 00:03:08 +0530

    feat: Python script to get POI information

--- python_scripts/wikipediaApp.py
+++ python_scripts/wikipediaApp.py
@@ -121,6 +121,31 @@ def generateFacts(information, fact_type):
         return {'error': 'An error occurred during generation'}, 500
 
 
+
+@app.route('/generatePOI', methods=['POST'])
+def generatePOI():
+    city_name = request.json.get('text')
+    payload = {
+        "model": "gemma:2b",
+        "prompt": "Create a JSON object that includes detailed information about the 4-5 most famous points of interest in the city " + city_name + ". The JSON object should be structured as follows: {'points_of_interest': [{'name': 'Name of the point of interest', 'details': 'Detailed information about the point of interest'}, {'name': 'Name of the point of interest', 'details': 'Detailed information about the point of interest'}, ...]}. The response should strictly adhere to the specified JSON format, with no additional symbols, newline characters, or extraneous information.",
+        "stream": False,
+    }
+    try:
+        response = requests.post(
+            "http://localhost:11434/api/generate", json=payload)
+
+        if response.status_code == 200:
+            response_data = response.text
+            cleaned_string = removeMarkdown(response_data)
+            parsed_data = json.loads(cleaned_string)
+            return (parsed_data)
+        else:
+            return jsonify({'error': 'An error occurred with the external request'}), response.status_code
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({'error': 'An error occurred during generation'}), 500
+
 @app.route('/getCityInformation', methods=['POST'])
 def getCityInformation():
     city_name = request.json.get('text')
