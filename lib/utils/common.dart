18820bec4b470619c59903d52fabf1abe99df689
Manas Dalvi <dalvimanas33@gmail.com>
2024-07-31 02:16:57 +0530

    feat: Download function

--- lib/utils/common.dart
+++ lib/utils/common.dart
@@ -1,6 +1,24 @@
+import 'package:background_downloader/background_downloader.dart';
 import 'package:flutter/material.dart';
 
 extension SizedBoxPadding on num {
   SizedBox get ph => SizedBox(height: toDouble());
   SizedBox get pw => SizedBox(width: toDouble());
+}
+
+downloadKml(String url, String filename, String directoryName) async {
+  await FileDownloader().download(
+    DownloadTask(
+      url: url,
+      filename: '$filename.kml',
+      updates: Updates.statusAndProgress,
+      directory: directoryName,
+      requiresWiFi: false,
+      retries: 5,
+      allowPause: true,
+    ),
+    onProgress: (progress) {
+      print(progress);
+    },
+  );
 }
